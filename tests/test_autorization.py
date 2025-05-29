a9f5b5b8bd0aa41da21238f89fc341a7c659fcb7
fuenukkusu2501 <fenixxx2501@gmail.com>
2025-05-29 14:40:12 +0300

    add pytest-rerunfailures in requirements.txt

--- tests/test_autorization.py
+++ tests/test_autorization.py
@@ -1,5 +1,3 @@
-import time
-
 import allure
 from playwright.sync_api import expect
 
@@ -15,12 +13,7 @@ def test_autorization(page_fixture, base_url):
         expect(page_fixture).to_have_url(f"{base_url}/profile")
     with allure.step("Проверяю, что профиль пользователь имеет корректный email"):
         expect(page_fixture).to_have_url(f"{base_url}/profile")
-    #email to have value=USER_EMAIL
 
-    # здесь будет проверка на то, что авторизация прошла успешно
+
     # Проверить "с возвращением"
-    # проверить, что страница сменилась на настройки, проверить что введенная почта соответствует той, что была введена
-
-def test_type_check(page_fixture):
-    from playwright.sync_api import Page
-    assert isinstance(page_fixture, Page)
+    # проверить что введенная почта соответствует той, что была введена #email to have value=USER_EMAIL
